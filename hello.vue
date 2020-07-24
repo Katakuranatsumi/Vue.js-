e71d0b0fe6e37aa0c01264364d065bfcd1e439e1
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-07-24 16:26:17 +0900

    hello.vueを追加

---
+++ hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1 class="greeting">メッセージ: {{ msg }}</h1>  
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return { msg: 'こんにちは！' }  
+  }
+}
+</script>
+
+<style>
+.greeting { color: #42b983; }
+</style>
