0a91c8cdf0ae3f98e71ad54ceb245f788d02e0fc
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-08-17 22:54:53 +0900

    CSSモジュールを使用したcss_module.vueを作成

---
+++ css_module.vue
@@ -0,0 +1,15 @@
+<template>
+  <p :class="$style.message">こんにちは！</p>
+</template>
+
+<script>
+export default {
+  created () {
+    // es-lint-disable-line no-console
+    console.log('css modules: $style', this.$style)  
+  }
+}
+</script>
+<style module>
+.message { color: #42b983; }
+</style>
